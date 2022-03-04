b502ef7428f7f5a1cd4109a9a79e2dc5972e70f4
Ortwin Gentz, FutureTap <futuretap@users.noreply.github.com>
2022-03-04 20:37:40 +0100

    Fixed exception in AA 2.4.0
    undefined method `delete' for :error:Symbol

--- lib/active_admin_delayed_job/admin/delayed_jobs.rb
+++ lib/active_admin_delayed_job/admin/delayed_jobs.rb
@@ -34,13 +34,13 @@ ActiveAdmin.register Delayed::Job, :as => "Background Job" do
     column :status do |job|
       case job.state
       when 'failed'
-        status_tag "Failed:", :error 
+        status_tag("Failed")
         span "#{job.last_error[0..100]}" 
       when 'running'
-        status_tag "Running", :warning
+        status_tag("Running")
         span "for #{time_ago_in_words(job.locked_at)} @ #{job.locked_by}"   
       when 'scheduled'
-        status_tag "Scheduled", :ok
+        status_tag("Scheduled")
         span "for #{time_ago_in_words(job.run_at)} from now" 
       else
         status_tag "Queued" 
